c3510876170016a27548bd5c9fcdab37b36c9b88
Fainsteve <fainstevesums@gmail.com>
2022-12-08 15:07:52 -0800

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Print a line of code using printf
+ *
+ * Description: 'this is c'
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
