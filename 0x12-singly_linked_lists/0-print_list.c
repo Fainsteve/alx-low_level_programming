567bcedb2d50592146c6ced67739309d95ee006c
Fainsteve <fainstevekassums@gmail.com>
2023-01-30 13:56:02 -0800

    12

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,26 +4,29 @@
 #include "lists.h"
 
 /**
- *  * print_list - check the code for Holberton School students.
- *   * @h: name of the list
- *    * Return: the number of nodes.
- *     */
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes.
+ */
 size_t print_list(const list_t *h)
 {
-		int count = 0;
+	    int count = 0;
 
-			while (h)
-					{
-								if (h->str == NULL)
-											{
-															printf("[0] (nil)\n");
-																	}
-										else
-													{
-																printf("[%d] %s\n", h->len, h->str);
-																		}
-												count++;
-														h = h->next;
-															}
-				return (count);
+				while (h)
+		{
+				if (h->str == NULL)
+			{
+				printf("[0] (nil)\n");
+			}
+			else
+						{
+							printf("[%d] %s\n", h->len, h->str);
+									}
+					count++;
+						h = h->next;
+							}
+
+	return (count);
 }
+
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,18 +4,17 @@
 #include "lists.h"
 
 /**
- *  * list_len - check the code for Holberton School students.
- *   * @h: name of the list
- *    * Return: the number of nodes.
- *     */
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
 size_t list_len(const list_t *h)
 {
 		int count = 0;
 
-			while (h)
-					{
-								count++;
-										h = h->next;
+		while (h)
+		{										count++;
+			h = h->next;
 											}
 				return (count);
 }
